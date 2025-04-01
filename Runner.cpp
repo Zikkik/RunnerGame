2aeae08e97c4796de12927985f751ee8fa39d4ad
Sebastian Zdziebko <sebastian.zdziebko17@gmail.com>
2025-04-01 22:24:23 +0200

    Setup character

--- Runner.cpp
+++ Runner.cpp
@@ -5,18 +5,28 @@ int main(){
     const int windowHeight{380}, windowWidth{512};
     
     // Rectangle dimensions, position and velocity
-    const int width{50}, height{80}, jumpVelocity{-22};
-    int posY{windowHeight - height}, velocity{0};
+    int const jumpVelocity{-22};
+    int velocity{0};
 
     // Acceleration due to gravity
     const int gravity{1};
 
-    // Check if character is in air
-    bool isInAir{false};
-
-
     // Initialize new window
     InitWindow(windowWidth, windowHeight, "Runner");
+
+    // Setup player character
+    Texture2D scarfy = LoadTexture("textures/scarfy.png");
+    Rectangle scarfyRec;
+        scarfyRec.width = scarfy.width/6;
+        scarfyRec.height = scarfy.height;
+        scarfyRec.x = 0;
+        scarfyRec.y = 0;
+    Vector2 scarfyPos;
+        scarfyPos.x = windowWidth/2 - scarfyRec.width/2;
+        scarfyPos.y = windowHeight - scarfyRec.height;
+
+    // Check if character is in air
+    bool isInAir{false};
 
     // Set frames per second
     SetTargetFPS(60);
@@ -28,7 +38,7 @@ int main(){
         ClearBackground(WHITE);
 
         // Ground check
-        if(posY >= windowHeight - height){
+        if(scarfyPos.y >= windowHeight - scarfyRec.height){
             // Rectangle in on the ground
             velocity = 0;
             isInAir = false;
@@ -39,15 +49,18 @@ int main(){
              isInAir = true;
         }
 
-        DrawRectangle(windowWidth/2, posY, width, height, BLUE);
+        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
+
+        // Jump check
         if(IsKeyPressed(KEY_SPACE) && !isInAir)
             velocity += jumpVelocity;
 
         // Update position
-        posY += velocity;
+        scarfyPos.y += velocity;
 
         // Stop drawing
         EndDrawing();
     }
+    UnloadTexture(scarfy);
     CloseWindow();
 }
